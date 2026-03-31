088cc40ddaa41f5cb70f00e6811f47c3a60b9abb
ersan <ersan@MacBook-Air-von-ersan.local>
2026-03-31 22:28:41 +0200

    project setup

---
+++ frontend/firebase-express-ts-app-4/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBWL035NbiQoboLkHvl2Xqfo63H5AbLaNk',
+    authDomain: 'fir-express-ts-app-4.firebaseapp.com',
+    projectId: 'fir-express-ts-app-4',
+    storageBucket: 'fir-express-ts-app-4.firebasestorage.app',
+    messagingSenderId: '959242699685',
+    appId: '1:959242699685:web:2e0aea8ac1da28eaf65e4c',
+  },
+};
